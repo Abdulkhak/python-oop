625b922c9ec4be5f57d10958cc2f32bf575dd6b9
Abdulkhak <a.abdulkhak@gmail.com>
2023-01-23 03:47:01 +0600

    feat: problems with patch

--- timer/timer_tests.py
+++ timer/timer_tests.py
@@ -1,10 +1,13 @@
-import unittest
 from timer import Timer 
 import time
+from mock import patch
+from unittest import TestCase
+from unittest.mock import patch
+import unittest
 
 class TestTimer(unittest.TestCase):
-
-    def test_timer_constructor(self):
+    @patch('time.sleep', return_value=None)
+    def test_timer_constructor(self, patched_time_sleep):
         t = Timer(10)
         self.assertEqual(t.get_time(), 10, "Should be 10")
         t.start()
@@ -24,6 +27,7 @@ class TestTimer(unittest.TestCase):
         t.resume()
         time.sleep(4)
         self.assertEqual(t.get_time(), 2, "Should be 2")  
+        patched_time_sleep.assert_called_once()
                 
 if __name__ == '__main__':
     unittest.main()

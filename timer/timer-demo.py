cb4f5a4cc6eeb238ec60f652749f7b47393ca8cc
Abdulkhak <a.abdulkhak@gmail.com>
2023-01-24 01:01:45 +0600

    fear: updated

--- timer/timer-demo.py
+++ timer/timer-demo.py
@@ -2,23 +2,11 @@ from timer import Timer
 import time
 
 def main():
-    t = Timer(100)
+    t = Timer(3)
     t.start() # starting timer
     print(t.get_time()) # 100 seconds
-    time.sleep(3)
-    print(t.get_time()) # 97 seconds
-    t.stop()
-    print(t.get_time()) # 0 seconds
-    t.start() # 100
     time.sleep(10)
-    print(t.get_time()) #90
-    t.pause()
-    print(t.get_time()) #90
-    time.sleep(3)
-    print(t.get_time()) # 90 seconds
-    t.resume()
-    time.sleep(5)
-    print(t.get_time()) # 85 seconds
-
+    print(t.get_time()) # 97 seconds
+    print(t.is_running())
 if __name__ == "__main__":
     main()

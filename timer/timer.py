60b0f5fc80e0bf87b3ad0cf7c3a2c6ba370802ee
Abdulkhak <a.abdulkhak@gmail.com>
2023-01-24 01:01:05 +0600

    feat: updated

--- timer/timer.py
+++ timer/timer.py
@@ -7,12 +7,19 @@ class Timer:
         self.__start_time = None
         self.__initial_second = second
         self.__second = second
+        self.expired = False
 
     def __calculate(self):
         self.__second = self.__second - int(time.time() - self.__start_time)
-
-    
+        if self.__second <= 0:
+            self.__stopped = True
+            self.__second = 0
+            self.__start_time = None
+            self.expired = True
+            
     def is_running(self):
+        if self.expired:
+            return False
         return not self.__stopped
 
     def get_time(self):
@@ -25,16 +32,25 @@ class Timer:
         self.__stopped = False
         self.__start_time = time.time()
         self.__second = self.__initial_second
+        self.expired = False
 
     def stop(self):
         self.__second = 0
         self.__stopped = True
         self.__start_time = None
+        self.expired = True
     
     def pause(self):
-        self.__start_time = None
-        self.__stopped = True
+        if self.is_running():
+            self.__stopped = True
+            self.__second = self.__second - int(time.time() - self.__start_time)
+            self.__start_time = None
+
 
     def resume(self):
-        self.__stopped = False
-        self.__start_time = time.time()
+        if self.__stopped:
+            self.__stopped = False
+            self.__second = self.__initial_second
+            self.__start_time = time.time()
+            self.expired = False
+

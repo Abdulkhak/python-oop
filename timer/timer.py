01d0c9ec637999aa088bbc3b42a1c795ab19d29d
Abdulkhak <a.abdulkhak@gmail.com>
2023-01-26 00:29:43 +0600

    feat: final

--- timer/timer.py
+++ timer/timer.py
@@ -3,51 +3,42 @@ import time
 
 class Timer:
     def __init__(self, second):
+        self.__stopped = True
         self.__start_time = None
         self.__initial_second = second
         self.__second = second
-        self.expired = False
-        self.paused = False
-        self.running = False
-
-    def start(self):
-        self.running = True
-        self.paused = False
-        self.expired = False
-        self.__start_time = time.time()
-        self.__second = self.__initial_second
-        self.__calculate()
-
-    def get_time(self):
-        return self.__second
-
-    def is_running(self):
-        return self.running and not self.expired
 
     def __calculate(self):
         self.__second = self.__second - int(time.time() - self.__start_time)
         if self.__second <= 0:
-            self.__stop()
-
-    def __stop(self):
-        self.__second = 0
-        self.running = False
-        self.expired = True
-        self.__start_time = None
-
-    def stop(self):
-        self.__stop()
-    
-    def pause(self):
-        if self.running:
-            self.paused = True
-            self.running = False
-            self.__second = self.__second - int(time.time() - self.__start_time)
+            self.__stopped = True
+            self.__second = 0
             self.__start_time = None
 
-    def resume(self):
-        if self.paused:
-            self.running = True
-            self.paused = False
-            self.__start_time = time.time()
 
+    def is_running(self):
+        return not self.__stopped
+
+    def get_time(self):
+        if self.__start_time != None:
+            self.__calculate()  
+        return self.__second
+
+
+    def start(self):
+        self.__stopped = False
+        self.__start_time = time.time()
+        self.__second = self.__initial_second
+
+    def stop(self):
+        self.__second = 0
+        self.__stopped = True
+        self.__start_time = None
+    
+    def pause(self):
+        self.__start_time = None
+        self.__stopped = True
+
+    def resume(self):
+        self.__stopped = False
+        self.__start_time = time.time()
